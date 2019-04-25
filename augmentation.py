b34e74d5ee5cfae3c4c98c4b1fe04f2da215f0c9
Valery Ilin <v.s.ilin@ya.ru>
2019-04-25 14:44:36 +0300

    +augmentation

---
+++ augmentation.py
@@ -0,0 +1,60 @@
+import Augmentor
+import tensorflow as tf
+import cv2
+import os
+
+additional_augmentation = 5
+folder = "augmentation_data/carrot_L1_001"
+video_number = "004"
+path_to_source = folder + "/output/"
+path_to_augment = folder + "/augment/"
+os.system("rm -rf {0}".format(path_to_augment))
+os.system("rm -rf {0}".format(path_to_source))
+
+
+p = Augmentor.Pipeline(folder)
+
+os.mkdir(path_to_augment)
+
+p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+p.zoom(probability=0.5, min_factor=1.1, max_factor=1.2)
+p.random_distortion(probability=1, grid_width=2, grid_height=2, magnitude=8)
+p.flip_top_bottom(0.5)
+p.flip_left_right(0.5)
+
+p.sample(additional_augmentation)
+
+p.process()
+
+
+
+
+lst = os.listdir(path_to_source)
+
+cur_number = 0
+for file in lst:
+    cur_class = file.split('_')[0]
+    cur_light = file.split('_')[1]
+    cur_source_file = file.split('_')[2]
+    cur_frame = file.split('_')[7]
+    #print(file)
+    #print(file.split('_'), cur_class, cur_number)
+    filename = path_to_augment+"aug_"+cur_class+"_"+cur_light+"_"+video_number+"_"+str(cur_number)+"_"+"from"+cur_source_file+"_"+cur_frame+".jpg"
+    print(filename)
+    image = cv2.imread(str(path_to_source)+str(file))
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    tf_img = tf.convert_to_tensor(rgb)
+
+    sat_image = tf.image.random_saturation(tf_img, lower=0.5, upper=1)
+    contrast_image = tf.image.random_contrast(sat_image, lower=0.5, upper=1)
+    brght_img = tf.image.random_brightness(contrast_image, max_delta=0.2)
+    jpg = tf.image.random_jpeg_quality(brght_img, min_jpeg_quality=50, max_jpeg_quality=100)
+
+    enc = tf.image.encode_jpeg(brght_img)
+    fwrite = tf.write_file(filename, enc)
+
+
+    with tf.Session() as sess:
+        sess.run(fwrite)
+
+    cur_number+=1
